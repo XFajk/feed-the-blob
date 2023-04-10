99ee78a8f4f35d30cb4bb92a6a5eb5c146478622
Rudolf Vrbensky <89221002+XFajk@users.noreply.github.com>
2023-04-11 01:32:03 +0200

    added a basic bunny that grows and a resizable functionalty

--- entities.py
+++ entities.py
@@ -8,19 +8,30 @@ class Feeder:
     def __init__(self, ds: tuple):
         self.ds = ds
 
-        self.position = pygame.Vector2(ds[0]/2, 60)
+        self.position = pygame.Vector2(ds[0] / 2, 60)
         self.radius = 20
         self.direction = 0  # 0 = no direction, 1 = right direction, -1 = left direction
+        self.img = pygame.image.load("assets/sprites/feeder.png").convert()
+        self.img.set_colorkey((255, 255, 255))
 
         self.particles = ShapeParticles("circle", 0.2)
 
     def draw(self, display, dt):
         self.particles.use(display, dt)
-        pygame.draw.circle(display, (0, 0, 255), self.position, self.radius)
+        pygame.draw.circle(display, (0, 0, 0), (self.position.x, self.position.y), 5, 2)
+        display.blit(self.img, (self.position.x-self.radius, self.position.y-self.radius+25))
 
     def update(self, dt, mouse_press, mouse_position):
         if mouse_press[0]:
             self.direction = mouse_position[0] - self.position.x
+            self.particles.add(
+                [self.position[0], self.position[1] + 45],
+                rnd.randint(0, 180),
+                rnd.randint(1, 3) / 10,
+                rnd.randint(1, 2) * 5,
+                (rnd.randint(100, 170), rnd.randint(100, 170), rnd.randint(100, 170)),
+                0.01
+            )
         else:
             self.direction += self.direction * -1 / 50
 
@@ -30,18 +41,67 @@ class Feeder:
             self.position.x = 21
             self.direction *= -1
 
-        if self.position.x > self.ds[0]-20:
+        if self.position.x > self.ds[0] - 20:
             self.position.x = self.ds[0] - 21
             self.direction *= -1
 
-        self.particles.add(
-            [self.position[0], self.position[1]+20],
-            rnd.randint(0, 180),
-            rnd.randint(1, 3) / 10,
-            rnd.randint(1, 2)*5,
-            (rnd.randint(100, 170), rnd.randint(100, 170), rnd.randint(100, 170)),
-            0.01
-        )
 
+class Bunny:
+    def __init__(self, ds):
+        self.ds = ds
 
+        self.radius = 32
+        self.position = pygame.Vector2(ds[0] / 2, ds[1] - ds[1] / 6 - self.radius)
 
+        self.max_radius = 100
+
+        self.mouth_collider = pygame.Rect(self.position.x - self.radius, self.position.y - self.radius, self.radius * 2, 10)
+        self.particles = ShapeParticles("circle", 0.2)
+
+        self.head_sprite = pygame.image.load("assets/sprites/basic bunny.png").convert()
+        self.head_sprite.set_colorkey((255, 255, 255))
+
+        self.alive = True
+
+    def draw(self, display, dt):
+        if self.alive:
+            pygame.draw.circle(display, (208, 208, 208), self.position, self.radius)
+            pygame.draw.circle(display, (0, 0, 0), self.position, self.radius, 1)
+            pygame.draw.ellipse(display, (254, 240, 240), (self.position.x, self.position.y-8, self.radius/2+self.radius/3, self.radius/2+10+self.radius/4))
+            pygame.draw.ellipse(display, (0, 0, 0), (self.position.x, self.position.y-8, self.radius/2+self.radius/3, self.radius/2+10+self.radius/4), 1)
+            display.blit(self.head_sprite, (self.position.x+self.radius/4, self.position.y-self.radius*1.2))
+        self.particles.use(display, dt)
+
+    def update(self, dt, foods):
+
+        self.mouth_collider = pygame.Rect(self.position.x - self.radius, self.position.y - self.radius, self.radius * 2, 10)  # update collider
+
+        # make movement
+
+        # collision logic
+        for food in foods:
+            if self.mouth_collider.collidepoint(food["loc"]) and self.radius < self.max_radius:
+                self.radius += 0.5*dt
+                self.position.y -= 0.5
+
+        if self.radius >= self.max_radius and self.alive:
+            for i in range(100):
+                self.particles.add(
+                    [self.position.x, self.position.y],
+                    rnd.randint(0, 360),
+                    rnd.randint(100, 2000)/100,
+                    rnd.randint(40, 60),
+                    (255, 255, 255),
+                    0.5
+                )
+
+            for i in range(100):
+                self.particles.add(
+                    [self.position.x, self.position.y],
+                    rnd.randint(0, 360),
+                    rnd.randint(100, 1500)/100,
+                    rnd.randint(20, 40),
+                    (rnd.randint(200, 220), rnd.randint(200, 220), rnd.randint(200, 220)),
+                    0.1
+                )
+            self.alive = False
--- main.py
+++ main.py
@@ -13,21 +13,25 @@ import entities
 def main() -> None:
     pygame.init()
 
-    ZOOM = 1
+    ZOOM = [1, 1]
     WS = 600, 600
-    DS = WS[0] // ZOOM, WS[1] // ZOOM
-    window = pygame.display.set_mode(WS, vsync=1)
+    ORIGIN_WS = 600, 600
+    current_ws = 600, 600
+    DS = WS[0] // ZOOM[0], WS[1] // ZOOM[0]
+    window = pygame.display.set_mode(WS, vsync=1, flags=RESIZABLE)
     display = pygame.Surface(DS)
     clock = pygame.time.Clock()
 
     # assets
 
     # logic variables
-    background_points = [[i] for i in range(-200, DS[1]+100, 100)]
+    background_points = [[i] for i in range(-200, DS[1] + 100, 100)]
     background_particles = ShapeParticles("circle", 0.0)
+    display_offset = [0, 0]
 
     # entities
     feeder = entities.Feeder(DS)
+    bunnies = [entities.Bunny(DS)]
 
     # timers
     last_time = time.perf_counter()
@@ -45,12 +49,22 @@ def main() -> None:
         # input methods
         keys = pygame.key.get_pressed()
         mouse_press = pygame.mouse.get_pressed()
-        mouse_position = [pygame.mouse.get_pos()[0]/ZOOM, pygame.mouse.get_pos()[1]/ZOOM]
+        """
+        in this line of code we calculate the mouse position
+        by multiplying it with ZOOM so it is on the correct position no matter what size the 
+        size of a window 
+        """
+        mouse_position = [
+            pygame.mouse.get_pos()[0] * ZOOM[0] - display_offset[0] * ZOOM[0],
+            pygame.mouse.get_pos()[1] * ZOOM[1] - display_offset[1] * ZOOM[1]
+        ]
 
         # logic of the game objects and entities
         if time.perf_counter() - bg_particle_timer > 0.1:
-            background_particles.add([0, DS[1]], rnd.randint(-90, 0), rnd.randint(1, 3), rnd.randint(60, 100), (200, 100, 0), 0.5)
-            background_particles.add([DS[0], 0], rnd.randint(90, 180), rnd.randint(1, 3), rnd.randint(60, 100), (200, 100, 0), 0.5)
+            background_particles.add([0, DS[1]], rnd.randint(-90, 0), rnd.randint(1, 3), rnd.randint(60, 100),
+                                     (200, 100, 0), 0.5)
+            background_particles.add([DS[0], 0], rnd.randint(90, 180), rnd.randint(1, 3), rnd.randint(60, 100),
+                                     (200, 100, 0), 0.5)
             bg_particle_timer = time.perf_counter()
 
         feeder.update(dt, mouse_press, mouse_position)
@@ -61,15 +75,24 @@ def main() -> None:
         # drawing the background
         background_particles.use(display, dt)
         for bp in background_points:
-            bp[0] += 2*dt
-            pygame.draw.line(display, (230, 122, 0), (-10, bp[0]), (DS[0]+10, bp[0]+100), 40)
-            if bp[0] > DS[1]+100:
+            bp[0] += 2 * dt
+            pygame.draw.line(display, (230, 122, 0), (-10, bp[0]), (DS[0] + 10, bp[0] + 100), 40)
+            if bp[0] > DS[1] + 100:
                 bp[0] = -200
 
         # drawing the feeder
         pygame.draw.line(display, (0, 0, 0), (0, 60), (DS[1], 60), 3)  # rail of the feeder
-
         feeder.draw(display, dt)
+
+        # drawing the bunnies
+        for b in sorted(bunnies, key=lambda x: x.radius, reverse=True):
+            b.update(dt, feeder.particles.objects)
+            b.draw(display, dt)
+            if not b.alive and b.particles.objects == []:
+                bunnies.remove(b)
+
+        # draw the bunny platform
+        pygame.draw.rect(display, (0, 255, 100), (0, DS[1] - DS[1] / 6, 600, 600))
 
         pygame.display.update()
 
@@ -77,11 +100,60 @@ def main() -> None:
         for event in pygame.event.get():
             if event.type == QUIT:
                 done = True
+            if event.type == VIDEORESIZE:
+                # resets the background
+                background_points = [[i] for i in range(-200, DS[1] + 100, 100)]
+                current_ws = event.w, event.h
+
+                # if  window is smaller than Original window size then scale the ZOOM so the mouse position fits
+                if event.w < ORIGIN_WS[0]:
+                    WS = event.w, WS[1]
+                    ZOOM[0] = ORIGIN_WS[0] / event.w
+                else:
+                    WS = ORIGIN_WS
+                    ZOOM[0] = 1
+                if event.h < ORIGIN_WS[1]:
+                    WS = WS[0], event.h
+                    ZOOM[1] = ORIGIN_WS[1] / event.h
+                else:
+                    WS = ORIGIN_WS
+                    ZOOM[1] = 1
+
+                """
+                if the window is bigger then the original window
+                size it scales the original resolution in this case
+                600X600 to a bigger one like 700x700 and adjust the ZOOM so the mouse 
+                position is correctly calculated
+                """
+                if event.w > ORIGIN_WS[0] and event.h > ORIGIN_WS[1]:
+                    if event.w > event.h:
+                        WS = event.h, event.h
+                        ZOOM = [ORIGIN_WS[0] / WS[0], ORIGIN_WS[1] / WS[1]]
+                    elif event.w < event.h:
+                        WS = event.w, event.w
+                        ZOOM = [ORIGIN_WS[0] / WS[0], ORIGIN_WS[1] / WS[1]]
+                    elif event.w == event.h:
+                        WS = event.w, event.w
+                        ZOOM = [ORIGIN_WS[0] / WS[0], ORIGIN_WS[1] / WS[1]]
 
         pygame.display.set_caption(f"feed the bunny FPS: {round(clock.get_fps(), 2)}")
 
+        """
+            this makes sure that if the window is bigger
+            in proportion to the resolution in this case 600x600
+            if it is it centers it on the axis that is bigger
+        """
+        if current_ws[0] > WS[0]:
+            display_offset[0] = (current_ws[0] - WS[0]) / 2
+        else:
+            display_offset[0] = 0
+        if current_ws[1] > WS[1]:
+            display_offset[1] = (current_ws[1] - WS[1]) / 2
+        else:
+            display_offset[1] = 0
+
         surf = pygame.transform.scale(display, WS)
-        window.blit(surf, (0, 0))
+        window.blit(surf, display_offset)
         clock.tick(61)
 
     pygame.quit()
